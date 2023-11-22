05db2a720c6a5ba93c903b7f540cf5051e78995c
matthewriegels <riegels.mj@oundleschool.org.uk>
2023-11-22 12:00:23 +0000

    added formatting to error messages bcoz ocd

--- addusers.php
+++ addusers.php
@@ -14,11 +14,11 @@ switch($_POST["role"]){
 		break;
 }
 
-echo("gender" . $_POST["gender"]."<br>");
-echo("forename" . $_POST["forename"]."<br>");
-echo("surname" . $_POST["surname"]."<br>");
-echo("passwd" . $_POST["passwd"]."<br>");
-echo("role" . $_POST["role"]."<br>");
+echo("gender: " . $_POST["gender"]."<br>");
+echo("forename: " . $_POST["forename"]."<br>");
+echo("surname: " . $_POST["surname"]."<br>");
+echo("passwd: " . $_POST["passwd"]."<br>");
+echo("role: " . $_POST["role"]."<br>");
 
 $stmt = $conn->prepare("INSERT INTO TblUsers (UserID,Gender,Surname,Forename,Password,Role)VALUES (null,:gender,:surname,:forename,:password,:role)");
 $stmt->bindParam(':forename', $_POST["forename"]);
