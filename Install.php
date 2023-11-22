b6d0eff56870c496f718274447b2c23c51bd99c6
matthewriegels <riegels.mj@oundleschool.org.uk>
2023-11-22 11:22:51 +0000

    install updated (should be complete)

--- Install.php
+++ Install.php
@@ -11,6 +11,22 @@ CREATE TABLE TblBooks (
 $stmt->execute();
 $stmt->closeCursor();
 
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblGenres;
+CREATE TABLE TblGenres (
+    GenreID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    GenreName VARCHAR(20) NOT NULL
+);");
+$stmt->execute();
+$stmt->closeCursor();
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblGenreLookup;
+CREATE TABLE TblGenreLookup (
+    GenreID INT(4),
+    BookID INT(4)
+);");
+$stmt->execute();
+$stmt->closeCursor();
+
 $stmt = $conn->prepare("DROP TABLE IF EXISTS TblUsers;
 CREATE TABLE TblUsers (
     UserID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
@@ -22,3 +38,20 @@ CREATE TABLE TblUsers (
 );");
 $stmt->execute();
 $stmt->closeCursor();
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblLoans;
+CREATE TABLE TblLoans (
+    BookID INT(4),
+    StudentID INT(4),
+    IssueDate DATE,
+    ReturnDate DATE
+);");
+$stmt->execute();
+$stmt->closeCursor();
+
+$stmt = $conn->prepare("DROP TABLE IF EXISTS TblAuthors;
+CREATE TABLE TblAuthors (
+    AuthorID INT(4) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Forename VARCHAR(30) NOT NULL,
+    Surname VARCHAR(30) NOT NULL
+);");
