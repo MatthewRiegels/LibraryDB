8c920dac8aa36f8565a4d8bb300f82ed25675cd6
matthewriegels <riegels.mj@oundleschool.org.uk>
2023-11-22 11:33:01 +0000

    added and adapted addusrs, users

---
+++ Users.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Users</title>
+    </head>
+    <body>
+        <a href="index.html">back to the homepage</a><br><br>
+        <!-- form for adding users -->
+        <form action="addusers.php" method = "post">
+            First name:<input type="text" name="forename"><br>
+            Last name:<input type="text" name="surname"><br>
+            Password:<input type="password" name="passwd"><br> <!-- add confirm password -->
+            Gender:<select name="gender">
+                <option value="M">Male</option>
+                <option value="F">Female</option>
+                <option value="O">Other</option>
+            </select><br>
+            <!--Next 3 lines create a radio button which we can use to select the user role-->
+            <input type="radio" name="role" value="NotAdmin" checked>No<br>
+            <input type="radio" name="role" value="Admin">Yes<br>
+            <input type="submit" value="Add User">
+        </form>
+
+        <!-- show all users -->
+        <?php
+        include_once('connection.php');
+        $stmt = $conn->prepare("SELECT * FROM TblUsers");
+        $stmt->execute();
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            switch($row["Role"]){
+                case 0:
+                    $role="";
+                    break;
+                case 1:
+                    $role=" (Admin)";
+                    break;
+                }
+            echo($row["Forename"] . ' ' . $row["Surname"] . $role . "<br>");
+        }
+        ?>
+    </body>
+</html>
---
+++ addusers.php
@@ -0,0 +1,31 @@
+<?php
+header('Location: users.php'); // if it breaks, comment this line first
+
+include_once("connection.php");
+
+array_map("htmlspecialchars", $_POST);
+
+switch($_POST["role"]){
+    case "NotAdmin":
+        $role=0;
+        break;
+	case "Admin":
+		$role=1;
+		break;
+}
+
+echo("gender" . $_POST["gender"]."<br>");
+echo("forename" . $_POST["forename"]."<br>");
+echo("surname" . $_POST["surname"]."<br>");
+echo("passwd" . $_POST["passwd"]."<br>");
+echo("role" . $_POST["role"]."<br>");
+
+$stmt = $conn->prepare("INSERT INTO TblUsers (UserID,Gender,Surname,Forename,Password,Role)VALUES (null,:gender,:surname,:forename,:password,:role)");
+$stmt->bindParam(':forename', $_POST["forename"]);
+$stmt->bindParam(':surname', $_POST["surname"]);
+$stmt->bindParam(':password', $_POST["passwd"]);
+$stmt->bindParam(':gender', $_POST["gender"]);
+$stmt->bindParam(':role', $role);
+$stmt->execute();
+$conn=null;
+?>
